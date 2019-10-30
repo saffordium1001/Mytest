b90499e9a7510b83878f1551f6fb786697983543
saffordium1001 <mr.safford93@gmail.com>
2019-10-30 13:48:02 -0400

    change parameters

--- README.c
+++ README.c
@@ -1,9 +1,7 @@
-#include<stdio.h>
-int main()
-{
-printf("hello world");
-printf("TEST!");
-return 0;
-}
+#pragma once
+
+int i = 6;
+//put more psuedo code here
+
 
 //more psuedo code to code up the program
